43f94c8ac9305339475cc3a01f7847cb6bac5a34
DavidCote <cotedavid343@gmail.com>
2020-06-04 16:04:54 -0500

    se inicializa el proyecto

---
+++ routes/index.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'David', carrera: 'ico',
+    accion:[{accion:"abrir"},
+            {accion:"cerrar"},
+            {accion:"No hacer nada"},
+            {accion:"dormir"}
+          ]
+  });
+});
+
+router.get('/hola',(req,res,next)=>{
+  res.render('home', {saludo:'hola mundo desde express handle bars'})
+});
+
+module.exports = router;
