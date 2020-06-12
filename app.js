7ed7593799ef5e69238f97736996811fa9e0c8c1
DavidCote <cotedavid343@gmail.com>
2020-06-12 01:58:16 -0500

    otra variable de entorno

--- app.js
+++ app.js
@@ -56,10 +56,5 @@ app.use(function(err, req, res, next) {
 
 //obtener localhost y puerto de variables
 const host = process.env.HOST || '0.0.0.0';
-const port = process.env.PORT || 3000;
-
-app.listen(port, host, ()=>{
-  console.log('El servidor esta funcionando');
-});
 
 module.exports = app;
--- routes/front_comic.js
+++ routes/front_comic.js
@@ -4,7 +4,7 @@ var request = require('request');
 
 //consumir http://localhost:3000/marvel/api/comic
 router.get('/listar',(req,res,next)=>{
-  request.get('http://localhost:3000/marvel/api/comic', (err,response,body)=>{
+  request.get(process.env.HOST, (err,response,body)=>{
     if(err) res.status(404).json({mensaje:"Error al consumir comic"});
     else res.render('comic_view',{'datos': JSON.parse(body)});
   });
